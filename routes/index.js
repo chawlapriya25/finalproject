4b8734f0a46414962489f94d1f8348674650e6a7
chawlapriya25 <chawlapriya25@gmail.com>
2020-09-06 17:38:41 -0600

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var app = require("../app"):
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+module.exports = app;
 module.exports = router;
