8b98ab0094f02346047d1b297e43af1f4cf5a30c
chawlapriya25 <chawlapriya25@gmail.com>
2020-09-09 17:46:19 -0600

    Changes

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
+var indexRouter = require('./routes/index');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+// connect db
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("were connected!");
+});
+
 
 var app = express();
 app.set('views',path.join(__dirname ,'views')); 
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// bode parser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json());
+// express session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }));
+
+app.use('/', indexRouter);
+
+var port = 5000;
+
+app.listen(port, function(){
+    console.log("Server started on port " + port);
+});
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var app = require("../app"):
+var app = require("../app");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ClothyStore' });
 });
 module.exports = app;
 module.exports = router;
