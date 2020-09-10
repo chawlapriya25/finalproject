4b9bf321384fba83a0dee20d9ec223d473ce8205
chawlapriya25 <chawlapriya25@gmail.com>
2020-09-09 18:03:13 -0600

    Add changes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ClothyStore' });
+  res.render('index', { title: 'killing it' });
 });
 module.exports = app;
 module.exports = router;
